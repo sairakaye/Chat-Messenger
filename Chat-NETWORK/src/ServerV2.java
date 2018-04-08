ecd16fba007a8f3cc1cbbf61fd89b6e643f33915
dich <34095473+idichaves@users.noreply.github.com>
2018-04-08 20:00:55 +0800

    changed server GUI to Judy Ann picture

--- Chat-NETWORK/src/ServerV2.java
+++ Chat-NETWORK/src/ServerV2.java
@@ -1,6 +1,3 @@
-import java.awt.BorderLayout;
-import java.awt.EventQueue;
-
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
@@ -15,16 +12,6 @@ import javax.swing.JTextField;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.net.InetAddress;
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.HashSet;
 
 public class ServerV2 extends JFrame {
 
@@ -47,7 +34,7 @@ public class ServerV2 extends JFrame {
 	public ServerV2() {
 		this.setTitle("The BuzzRoom's Server");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 775);
+		setBounds(100, 100, 832, 556);
 
 		contentPane = new JPanel();
 		contentPane.setBackground(Color.WHITE);
@@ -60,26 +47,19 @@ public class ServerV2 extends JFrame {
 		serverLog.setBounds(10, 207, 414, 318);
 		
 		JScrollPane scrollPane = new JScrollPane(serverLog);
-		scrollPane.setBounds(10, 352, 414, 373);
+		scrollPane.setBounds(10, 139, 278, 358);
 		contentPane.add(scrollPane);
 		
 		JLabel serverLogoLbl = new JLabel("Server logo");
-		serverLogoLbl.setIcon(new ImageIcon("img\\LogoServer.png"));
-		serverLogoLbl.setBounds(140, 54, 150, 150);
+		serverLogoLbl.setIcon(new ImageIcon("img\\Judy Ann.JPG"));
+		serverLogoLbl.setBounds(292, 58, 510, 439);
 		contentPane.add(serverLogoLbl);
-		
-		JLabel lblNewLabel = new JLabel("The BuzzRoom's Server");
-		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
-		lblNewLabel.setForeground(new Color(0, 0, 128));
-		lblNewLabel.setFont(new Font("Trebuchet MS", Font.BOLD, 24));
-		lblNewLabel.setBounds(10, 11, 414, 29);
-		contentPane.add(lblNewLabel);
 		
 		txtLocalhost = new JTextField();
 		txtLocalhost.setHorizontalAlignment(SwingConstants.CENTER);
 		txtLocalhost.setFont(new Font("Tahoma", Font.PLAIN, 14));
 		txtLocalhost.setText("localhost");
-		txtLocalhost.setBounds(10, 239, 414, 29);
+		txtLocalhost.setBounds(10, 55, 278, 29);
 		contentPane.add(txtLocalhost);
 		txtLocalhost.setColumns(10);
 
@@ -94,20 +74,13 @@ public class ServerV2 extends JFrame {
 		btnRun.setFont(new Font("Trebuchet MS", Font.BOLD, 12));
 		btnRun.setBackground(new Color(0, 0, 128));
 		btnRun.setForeground(new Color(255, 255, 255));
-		btnRun.setBounds(10, 279, 189, 29);
+		btnRun.setBounds(10, 97, 118, 29);
 		contentPane.add(btnRun);
 
 		JLabel lblServerIpAddress = new JLabel("Server IP Address:");
 		lblServerIpAddress.setFont(new Font("Trebuchet MS", Font.BOLD, 12));
-		lblServerIpAddress.setBounds(161, 211, 118, 29);
+		lblServerIpAddress.setBounds(96, 13, 118, 29);
 		contentPane.add(lblServerIpAddress);
-		
-		JLabel lblUnavailable = new JLabel("Invalid IP Address!");
-		lblUnavailable.setForeground(new Color(128, 0, 0));
-		lblUnavailable.setHorizontalAlignment(SwingConstants.CENTER);
-		lblUnavailable.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		lblUnavailable.setBounds(10, 319, 414, 22);
-		contentPane.add(lblUnavailable);
 		
 		JButton btnStop = new JButton("Stop Server");
 		btnStop.addActionListener(new ActionListener() {
@@ -118,7 +91,7 @@ public class ServerV2 extends JFrame {
 		btnStop.setForeground(Color.WHITE);
 		btnStop.setFont(new Font("Trebuchet MS", Font.BOLD, 12));
 		btnStop.setBackground(new Color(0, 0, 128));
-		btnStop.setBounds(235, 279, 189, 29);
+		btnStop.setBounds(170, 97, 118, 29);
 		contentPane.add(btnStop);
 	}
 }
